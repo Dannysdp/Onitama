30beae13f1f20c03ebdd092c962ab52bc085ff98
Pedro Scheeffer <pedrosd26@gmail.com>
2021-06-26 17:55:02 -0300

    coments

--- Onitama.hs
+++ Onitama.hs
@@ -106,9 +106,11 @@ showGame g = show g --TODO
 
 showAction :: OnitamaAction -> String
 showAction (OnitamaAction (a,b) card (c,d)) = ("(" ++ show a ++ "," ++ show b ++ ") " ++ show card ++ " (" ++show c ++","++ show d ++ ")")
-   
+
+-- "(OnitamaAction (posicion incial x, posicion incial y) carta (posicion final x, posicion final y))"
+
 readAction :: String -> OnitamaAction
-readAction texto = (OnitamaAction (1,1) Tiger (1,0)) --TODO (OnitamaAction (int,int) carta (int,int)) 
+readAction texto = (OnitamaAction (1,1) Tiger (1,0)) --TODO
 
 players :: [OnitamaPlayer]
 players = []
